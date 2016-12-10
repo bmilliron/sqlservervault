e2f78d3ab790d323b26c7e81638f8e32b83e432a
Ben Milliron <bmilliron@hotmail.com>
2016-12-09 19:04:58 -0500

    Update logins.sql
    Remove condition.

--- administration/logins.sql
+++ administration/logins.sql
@@ -4,4 +4,4 @@ sqlcmd -S <server name> -E -d master -Q  "EXEC sp_help_revlogin" -o "build_login
 
 --drop script
 
-sqlcmd -S <server name> -E -d master -Q  "select 'DROP LOGIN ' + [name] from sys.server_principals where type_desc = 'SQL_LOGIN' and sid <> 0x01 and substring(name, 1, 1) <> '#' and name <> 'oitag'" -o "drop_logins.sql" -h-1 -w 700
+sqlcmd -S <server name> -E -d master -Q  "select 'DROP LOGIN ' + [name] from sys.server_principals where type_desc = 'SQL_LOGIN' and sid <> 0x01 and substring(name, 1, 1) <> '#' " -o "drop_logins.sql" -h-1 -w 700
